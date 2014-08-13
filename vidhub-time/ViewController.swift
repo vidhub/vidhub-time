51bdbadbf5cb807938dfae19f4decdf96f300f54
Adam Zmenak <adam@dinteractive.ca>
2014-08-13 19:18:18 -0400

    Initial Commit

---
+++ vidhub-time/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  vidhub-time
+//
+//  Created by Adam Zmenak on 2014-08-13.
+//  Copyright (c) 2014 Vidhub. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+                                    
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+                                    
+    }
+
+
+}
+
---
+++ vidhub-time/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  vidhub-time
+//
+//  Created by Adam Zmenak on 2014-08-13.
+//  Copyright (c) 2014 Vidhub. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
